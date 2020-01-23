1cdbedab7f652d254ded16083170f24515e4af7b
lilsakabighurt <khalilhall1221@outlook.com>
2020-01-22 20:39:49 -0500

    call stars api

--- api.js
+++ api.js
@@ -5,7 +5,7 @@ module.exports = api;
 
 
 
-//function to make api call to github
+//function to make api call to github api
 function userName(username){
     axios.get(`https://api.github.com/users/${username}?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}`)
     .catch(err=>{
@@ -14,4 +14,17 @@ function userName(username){
     }).then(res=>{
         console.log(res.data)
     })
-} userName("lilsakabighurt")
+}// userName("lilsakabighurt")
+
+//api call to get number of stars//
+function stars(username){
+    axios.get(`https://api.github.com/users/${username}/repos?client_id=${
+        process.env.CLIENT_ID
+      }&client_secret=${process.env.CLIENT_SECRET}&per_page=100`)
+      .catch(err=>{
+          console.log("no stars bruh");
+          process.exit(1);
+      }).then(res=>{
+        console.log(res);
+      })
+} stars("lilsakabighurt");
